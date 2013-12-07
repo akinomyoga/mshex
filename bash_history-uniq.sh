18215a5ae25d1d48bcf2d180032ac11650e8c310
Koichi Murase <myoga.murase@gmail.com>
2013-12-08 00:09:25 +0900

    v20131208

--- bash_history-uniq.sh
+++ bash_history-uniq.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-cat $HOME/.bash_history |
+tac $HOME/.bash_history | {
+  declare -A lines
+  declare line
+  while read -r line; do
+    test -n "${lines[x$line]}" && continue
+    lines["x$line"]=1
+    echo "$line"
+  done
+} | tac
