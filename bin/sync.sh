9b5176495ec7eaeca4bcf47aa3740bc3e086c92f
Koichi Murase <myoga.murase@gmail.com>
2024-04-11 19:03:09 +0900

    sync.sh: set terminal title

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,10 +1,37 @@
 #!/usr/bin/env bash
 
+function sync/set-title {
+  local title=${1//[! -~]/#}
+  if [[ $STY && $WINDOW ]]; then
+    # we are probably in screen
+    printf '\ek%s\e\\' "$1" >&2
+  else
+    printf '\e]0;%s\e\\' "$1" >&2
+  fi
+}
+
+function sync/finalize {
+  sync/set-title '[sync.sh end]'
+}
+function sync/trapint {
+  sync/finalize
+  trap - INT
+  kill -INT $$
+}
+function sync/set-up-finalizer {
+  trap -- sync/finalize EXIT
+  trap -- sync/trapint INT
+}
+
 function main {
   if [[ $1 != *:* ]]; then
     return 1
   fi
+
   local host=${1%%:*} path=${1#*:} ntry=0
+  sync/set-title "[sync.sh ${path##*/} @ $host]"
+  sync/set-up-finalizer
+
   rsync -avz "$1" "$2"
   while
     rsync -avz "$1" "$2" --rsync-path="inotifywait -q -e modify -e moved_to '$path' >/dev/null; sleep 3; rsync"
