41b30b3e18d31d0948356480ccae62b3f05470bb
Koichi Murase <myoga.murase@gmail.com>
2024-04-11 09:43:18 +0900

    sync.sh: immediately exit with SIGINT

--- bin/sync.sh
+++ bin/sync.sh
@@ -7,9 +7,18 @@ function main {
   local host=${1%%:*} path=${1#*:} ntry=0
   rsync -avz "$1" "$2"
   while
-    rsync -avz "$1" "$2" --rsync-path="inotifywait -q -e modify -e moved_to '$path' >/dev/null; sleep 3; rsync" && ((ntry=0))
+    rsync -avz "$1" "$2" --rsync-path="inotifywait -q -e modify -e moved_to '$path' >/dev/null; sleep 3; rsync"
+    local ext=$?
+
+    # SIGINT: rsync seems to exit with status 20 when it receive SIGINT,
+    # SIGTERM, or SIGHUP.  Also, for the most cases, it somehow returns 255
+    # (unexplained error) for SIGINT.
+    ((ext==130||ext==20||ext==255)) && return 130
+
+    ((ext==0)) && ((ntry=0))
     ((ntry++<5)) # 5回連続で失敗したら終了
   do
+    echo "sync.sh: rsync exit=$ext" >&2
     sleep 1
   done
   return 1
